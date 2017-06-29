92f75c7e106639d1021a9487219fd906a64c82ef
davlatbek <idavlatbek19@gmail.com>
2017-06-30 02:24:38 +0300

    update temp data class

--- app/src/main/java/innopolis/studentsapp/utilities/TempData.java
+++ app/src/main/java/innopolis/studentsapp/utilities/TempData.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import innopolis.studentsapp.R;
 import innopolis.studentsapp.entities.Group;
 import innopolis.studentsapp.entities.Semester;
 import innopolis.studentsapp.entities.Student;
@@ -15,15 +16,24 @@ import innopolis.studentsapp.entities.Student;
 public class TempData {
     private static TempData data = null;
     public static List<Group> groups = null;
+
+    public static List<Student> getStudents() {
+        return students;
+    }
+
     public static List<Student> students = null;
 
-    private TempData(){
+    static {
         students = new ArrayList<>();
-        students.add(new Student("david", "beckham", "nomiddename", new Date(1234000L), 1L));
-        students.add(new Student("cristian", "verner", "nomiddename", new Date(12345678L), 1L));
-        students.add(new Student("davlet", "israilov", "nomiddename", new Date(123457689L), 1L));
+        students.add(new Student("Lillie!!!", "clinton" , "kurbonovich", new Date(123456789L), 1L, R.drawable.portraitphoto));
+        students.add(new Student("albert", "einstein" , "kurbonovich", new Date(123456789L), 1L, R.drawable.einstein));
+        students.add(new Student("lillie", "clinton" , "kurbonovich", new Date(123456789L), 1L, R.drawable.lillie));
+        students.add(new Student("albert", "einstein" , "kurbonovich", new Date(123456789L), 1L, R.drawable.einstein));
+        students.add(new Student("lillie", "clinton" , "kurbonovich", new Date(123456789L), 1L, R.drawable.lillie));
+    }
 
-        Group group = new Group(1L, "group p1" , 10, Semester.FALL);
+    private TempData(){
+        Group group = new Group(1L, "group portraitphoto" , 10, Semester.FALL);
         group.setStudentList(students);
 
         groups = new ArrayList<>();
@@ -36,5 +46,13 @@ public class TempData {
         if (data == null)
             return new TempData();
         return data;
+    }
+
+    public Student getStudentById(Long id){
+        for (Student student:students){
+            if (student.getId().equals(id))
+                return student;
+        }
+        return null;
     }
 }
