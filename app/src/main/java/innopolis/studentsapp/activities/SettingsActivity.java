a44cdb36d941a4af3692cfc2137ba9827f8b3bcd
davlatbek <idavlatbek19@gmail.com>
2017-07-06 12:43:34 +0300

    save_passwod setting disables when save_login changes

--- app/src/main/java/innopolis/studentsapp/activities/SettingsActivity.java
+++ app/src/main/java/innopolis/studentsapp/activities/SettingsActivity.java
@@ -1,7 +1,11 @@
 package innopolis.studentsapp.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
 import innopolis.studentsapp.R;
@@ -10,10 +14,44 @@ import innopolis.studentsapp.R;
  * Created by davlet on 7/6/17.
  */
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preference_layout);
+//        ListPreference listPreference = (ListPreference) findPreference("language_preference");
+//        listPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+//            @Override
+//            public boolean onPreferenceChange(Preference preference, Object newValue) {
+//                SharedPreferences sharedPreferences =
+//                        PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+//                sharedPreferences.edit().putString("language", newValue.toString()).apply();
+//                return true;
+//            }
+//        });
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //reset password, when checkbox save login is disabled
+        if (key.equals("save_login")){
+            Preference preference = findPreference("save_password");
+            preference.setDefaultValue(false);
+            sharedPreferences.edit().putBoolean("save_password", false).apply();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
     }
 }
